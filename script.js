ddef0a6df1995f9dae767f98d45090d4fd7141db
certificatedatascience <51219330+certificatedatascience@users.noreply.github.com>
2019-06-01 16:55:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,7 @@
 function load_footer() {
     $.getJSON( "https://certificatedatascience.github.io/common_data.json", 
          function( data ) {
-    //console.log(data);
-                    $("#footer").html(data["footer"]);
-                    $("#sidebar_container").html(data["sidebar"]);
-    });
+            $("#footer").html(data["footer"]);
+          }
+    );
 }
