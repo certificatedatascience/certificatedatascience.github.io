8c91199552683d59e825bfed41e63656d6887ad2
certificatedatascience <51219330+certificatedatascience@users.noreply.github.com>
2019-06-01 17:35:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function load_footer() {
     $.getJSON( "https://certificatedatascience.github.io/common_data.json", 
          function( data ) {
             $("#footer").html(data["footer"]);
-        
+            alert(data["sidebar"]);
             $("#sidebar_container").html(data["sidebar"]);
           }
     );
